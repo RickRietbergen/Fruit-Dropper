381662448692ce0c5750ea9cb6e7267660c5db98
ThijmenJava <1194425@student.roc-nijmegen.nl>
2023-06-26 09:47:09 +0200

    setup

---
+++ Food.java
@@ -0,0 +1,18 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Food here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Food extends Actor {
+    
+    public void act() {
+        
+    }
+    
+    public void sinkToTheBottom() {
+        int y = getY();
+    }
+}
